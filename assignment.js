26bdde8189ce577afa38e09ea989b10119742a87
Mahabub Alam <green.mahabub1971@gmail.com>
2021-01-22 02:02:03 +0600

    Third Problem Solved

--- assignment.js
+++ assignment.js
@@ -1,10 +1,12 @@
-// Kilometer to meter converter calculator.
+// First Problem: Kilometer to meter conversion calculator.
+
+// we know, 1 kilometer = 1000 meter.
+
 function kilometerToMeter(kilometer) {
-    // checking, whether the input is a string or not.
     if (typeof kilometer === "string") {
         return "Distance value can't be a string. Please insert a number.";
     } else if (kilometer >= 0) {
-        var meter = kilometer * 1000; // we know, 1 kilometer = 1000 meter.
+        var meter = kilometer * 1000;
         return meter;
     } else {
         return "Distance can't be negative.";
@@ -14,12 +16,13 @@ function kilometerToMeter(kilometer) {
 var resultOne = kilometerToMeter(5);
 console.log(resultOne);
 
-// budget calculator
+// Second Problem: creating a budget calculator.
 
 // Given values are listed below:
-// 1 watch price = 50 dollar
-// 1 phone price = 100 dollar
-// 1 laptop price = 500 dollar
+
+// per watch price is = 50 dollar
+// per phone price is = 100 dollar
+// per laptop price is = 500 dollar
 
 function budgetCalculator(watch, phone, laptop) {
     if (
@@ -43,3 +46,38 @@ function budgetCalculator(watch, phone, laptop) {
 }
 var resultTwo = budgetCalculator(100, 120, 150);
 console.log(resultTwo);
+
+// Third Problem: hotelCost calculation.
+
+// Given values are listed below:
+
+//  for first 10 days (1 to 10), per day staying cost is = 100 taka
+//  for second 10 days (11 to 20), per day staying cost is = 80 taka
+//  for rest of the days (21 to unlimited), per day staying cost is = 50 taka
+
+function hotelCost(days) {
+    var totalCost = 0;
+
+    if (typeof days === "string") {
+        return "Days value can't be a string. Please insert zero (0) or any positive number.";
+    } else if (days < 0) {
+        return "Days can't be negative. Please insert zero (0) or any positive number.";
+    } else if (days <= 10) {
+        totalCost = days * 100;
+    } else if (days <= 20) {
+        var firstTenDaysRent = 10 * 100;
+        var remaining = days - 10;
+        var secondTenDaysRent = remaining * 80;
+        totalCost = firstTenDaysRent + secondTenDaysRent;
+    } else {
+        var firstTenDaysRent = 10 * 100;
+        var secondTenDaysRent = 10 * 80;
+        var remaining = days - 20;
+        var restOfTheDaysRent = remaining * 50;
+        totalCost = firstTenDaysRent + secondTenDaysRent + restOfTheDaysRent;
+    }
+    return totalCost;
+}
+
+var resultThree = hotelCost(25);
+console.log(resultThree);
